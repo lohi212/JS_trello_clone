a15f2e84ae3340eac07864a9026a144d0c1163c2
lohi <suvvarilohita@gmail.com>
2021-01-25 17:53:59 +0530

    creation of cards and columns with input data

--- public/app.js
+++ public/app.js
@@ -16,35 +16,62 @@ function allowDrop(ev) {
     ev.target.appendChild(document.getElementById(data));
   }
 
-(function() {
-    const url="/test-url";
-    fetch(url)
-	.then(response => response.json())
-	.then(data => {
-        console.log(data);
-        const contnr = document.querySelector("#container");
-        const cols1 = document.querySelector("#column1");
-        const cols2 = document.querySelector("#column2");
-        const cols3 = document.querySelector("#column3");
+  function createColumn()
+  {
+    var col = document.createElement('div');
+    const inp_col_data = document.getElementById('ip_col_data');
+    col.innerHTML = `<div class = "head">${inp_col_data.value}</div>`;
+    col.classList.add('column');
+    const contnr = document.querySelector("#container");
+    contnr.appendChild(col);
+  }
 
-        data.tasks.forEach(element => {
-            var card=document.createElement('div');
-            card.innerHTML = element.name;
-            card.classList.add('card');
-            if(element.status === 'IN_PROGRESS'){
-                cols2.appendChild(card);
-            } else if(element.status === 'TODO'){
-                cols1.appendChild(card);
-            }else {
-                cols3.appendChild(card);
-            }
+  function createCard(id)
+  {
+    // const place = document.getElementById('card_up');
+    const contnr = document.querySelector("#container");
+    const cols3 = document.querySelector(id);
+    const inp_data = document.getElementById('ip_data');
+    const inp_val = inp_data.value;
+    var card = document.createElement('div');
+    // card.innerHTML = `<div class="card" id="cd1" draggable="true" ondragstart="drag(event)"></div>`;
+    card.innerHTML = inp_val;
+    // 
+    card.classList.add('card');
+    cols3.appendChild(card);
+    contnr.appendChild(cols3);
+  }
+
+// (function() {
+//     const url="/test-url";
+//     fetch(url)
+// 	.then(response => response.json())
+// 	.then(data => {
+//         console.log(data);
+//         const contnr = document.querySelector("#container");
+//         const cols1 = document.querySelector("#column1");
+//         const cols2 = document.querySelector("#column2");
+//         const cols3 = document.querySelector("#column3");
+
+//         // createCard(cols1);
+//         data.tasks.forEach(element => {
+//             var card=document.createElement('div');
+//             card.innerHTML = element.name;
+//             card.classList.add('card');
+//             if(element.status === 'IN_PROGRESS'){
+//                 cols2.appendChild(card);
+//             } else if(element.status === 'TODO'){
+//                 cols1.appendChild(card);
+//             }else {
+//                 cols3.appendChild(card);
+//             }
             
             
-        });
-        contnr.appendChild(cols1);
-        contnr.appendChild(cols2);
-        contnr.appendChild(cols3);
+//         });
+//         contnr.appendChild(cols1);
+//         contnr.appendChild(cols2);
+//         contnr.appendChild(cols3);
 
-    });
+//     });
     
-})();
+// })();
--- server.js
+++ server.js
@@ -2,18 +2,35 @@ const express = require('express');
 const app = express();
 app.use(express.static('public/'));
 
-const tasks = [{
-    name: 'Task 1',
-    status: 'TODO',
-},
-{
-    name: 'Task 2',
-    status: 'IN_PROGRESS',
-},
-{
-    name: 'Task 3',
-    status: 'DONE',
-}
+const tasks =[
+    {
+ 		"name": "task 1",
+ 		"status": "TODO"
+	 },
+	 {
+		"name": "task 1",
+		"status": "TODO"
+	},
+	{
+		"name": "task xyz",
+		"status": "IN_PROGRESS"
+	},
+	{
+		"name": "task pqr",
+		"status": "TODO"
+	},
+	{
+		"name": "task 345",
+		"status": "TODO"
+	},
+ 	{
+ 		"name": "Task 2",
+ 		"status": "IN_PROGRESS"
+ 	},
+ 	{
+ 		"name": "Task 3",
+ 		"status": "DONE"
+ 	}
 ];
 app.get('/test-url', (req, res) => {
     res.json({ tasks });
