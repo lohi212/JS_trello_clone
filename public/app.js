f789e279356e5cbed44a464c87e38bc48faf4044
lohi <suvvarilohita@gmail.com>
2021-02-21 23:00:26 +0530

    dynamic column addition with cards

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@
 
 // IIFE - Immediatly Invoked Function Expression
 // Anonymous function - 
-function allowDrop(ev) {
+  function allowDrop(ev) { 
     ev.preventDefault();
   }
   
@@ -15,32 +15,67 @@ function allowDrop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
   }
-
+  // function createColumn()
+  // {
+  //   var col = document.createElement('div');
+  //   const inp_col_data = document.getElementById('ip_col_data');
+  //   col.innerHTML = `<div class = "head">${inp_col_data.value}</div>`;
+  //   col.classList.add('column');
+  //   const contnr = document.querySelector("#container");
+  //   contnr.appendChild(col);
+  // }
   function createColumn()
   {
-    var col = document.createElement('div');
-    const inp_col_data = document.getElementById('ip_col_data');
-    col.innerHTML = `<div class = "head">${inp_col_data.value}</div>`;
-    col.classList.add('column');
     const contnr = document.querySelector("#container");
-    contnr.appendChild(col);
+    var col = document.getElementById('columns-creation');
+    const inp_col_data = document.getElementById('ip_col_data');
+    var card=document.createElement('div');
+    card.innerHTML=`<input type="text" id="ip_data_${+new Date}">
+    <div id="cards-container-${+new Date}"></div>
+    <button type = "button" onclick="createCard('${+new Date}');">Add card</button>`;
+    var header = document.createElement('div');
+    header.className="head";
+    header.innerHTML =`${inp_col_data.value}`;
+    inp_col_data.value='';
+    console.log(header);
+    var cols = document.createElement('div');
+    cols.className="column";
+    cols.id = `column${+new Date}`;
+    cols.ondrop="drop(event)";
+    cols.ondragover="allowDrop(event)";    
+    cols.appendChild(header);
+    cols.appendChild(card);
+    // cols.setAttribute("style","height:auto;")
+    contnr.appendChild(cols); 
   }
+  
+  
 
   function createCard(id)
   {
     // const place = document.getElementById('card_up');
     const contnr = document.querySelector("#container");
-    const cols3 = document.querySelector(id);
-    const inp_data = document.getElementById('ip_data');
+    const cols = document.getElementById(`cards-container-${id}`);
+    const inp_data = document.getElementById(`ip_data_${id}`);
     const inp_val = inp_data.value;
     var card = document.createElement('div');
-    // card.innerHTML = `<div class="card" id="cd1" draggable="true" ondragstart="drag(event)"></div>`;
+    card.setAttribute('draggable', true);
+    card.addEventListener('dragstart', (evt) => {
+      drag(evt);
+    });
     card.innerHTML = inp_val;
-    // 
+    inp_data.value = '';
+    card.id = +new Date;
     card.classList.add('card');
-    cols3.appendChild(card);
-    contnr.appendChild(cols3);
+    cols.appendChild(card);
+    // contnr.appendChild(cols);
   }
+
+  (function() {
+    localStorage.setItem(`${id}`,document.getElementById(`${id}`).value);
+
+    document.getElementById(`${id}`).value = localStorage.getItem(`${id}`);
+  });
 
 // (function() {
 //     const url="/test-url";
